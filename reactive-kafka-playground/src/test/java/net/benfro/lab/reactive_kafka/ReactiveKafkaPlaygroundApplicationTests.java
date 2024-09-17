d366ef54871c0a08c1bc6b7bd785cd44804d24ef
benfro <54681232+benfro@users.noreply.github.com>
2024-09-17 18:36:22 +0200

    Added first

---
+++ reactive-kafka-playground/src/test/java/net/benfro/lab/reactive_kafka/ReactiveKafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package net.benfro.lab.reactive_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveKafkaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
