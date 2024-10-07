50a85dbe3ccbe05648a82a98aee9729293e7e857
Bengt-Erik Fröberg <bengt-erik.froeberg@redpill-linpro.com>
2024-10-07 11:30:11 +0200

    Consumer done

---
+++ src/main/java/net/benfro/lab/reactive_kafka/consumer/Lec01Consumer.java
@@ -0,0 +1,37 @@
+package net.benfro.lab.reactive_kafka.consumer;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+@Slf4j
+public class Lec01Consumer {
+
+    public static void main(String[] args) {
+
+        var consumerConfig = Map.<String, Object>of(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+            ConsumerConfig.GROUP_ID_CONFIG, "demo-group-123",
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+            ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "4077"
+        );
+
+        var options = ReceiverOptions.create(consumerConfig)
+            .subscription(List.of("order-events"));
+        //.consumerProperty(ConsumerConfig.GROUP_ID_CONFIG, "demo-group")
+
+        KafkaReceiver.create(options)
+            .receive()
+            .doOnNext(r -> log.info("r.key: {}, r.value: {}", r.key(), r.value()))
+            .doOnNext(r -> r.receiverOffset().acknowledge() )
+            .subscribe();
+    }
+}
---
+++ src/main/java/net/benfro/lab/reactive_kafka/consumer/Lec02Consumer.java
@@ -0,0 +1,38 @@
+package net.benfro.lab.reactive_kafka.consumer;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+@Slf4j
+public class Lec02Consumer {
+
+    public static void main(String[] args) {
+
+        var consumerConfig = Map.<String, Object>of(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+            ConsumerConfig.GROUP_ID_CONFIG, "inventory-service-group",
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+            ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "4077"
+        );
+
+        var options = ReceiverOptions.create(consumerConfig)
+            .subscription(Pattern.compile("order.*"));
+//            .subscription(List.of("order-events"));
+        //.consumerProperty(ConsumerConfig.GROUP_ID_CONFIG, "demo-group")
+
+        KafkaReceiver.create(options)
+            .receive()
+            .doOnNext(r -> log.info("topic: {}, key: {}, value: {}", r.topic(), r.key(), r.value()))
+            .doOnNext(r -> r.receiverOffset().acknowledge())
+            .subscribe();
+    }
+}
