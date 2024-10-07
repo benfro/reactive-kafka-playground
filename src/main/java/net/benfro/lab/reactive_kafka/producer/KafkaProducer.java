ff353eb4bcfda3a9d610d168f1b8b54ed72338fe
Bengt-Erik Fröberg <bengt-erik.froeberg@redpill-linpro.com>
2024-10-07 12:38:51 +0200

    Producer

---
+++ src/main/java/net/benfro/lab/reactive_kafka/producer/KafkaProducer.java
@@ -0,0 +1,42 @@
+package net.benfro.lab.reactive_kafka.producer;
+
+import java.time.Duration;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderRecord;
+
+@Slf4j
+public class KafkaProducer {
+
+    public static void main(String[] args) {
+
+        Map<String, Object> config = Map.<String, Object>of(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+        );
+
+        SenderOptions<String, String> senderOptions = SenderOptions.<String, String>create(config);
+//        KafkaSender<String, String> sender = KafkaSender.create(senderOptions);
+
+        var flux = Flux.interval(Duration.ofMillis(100))
+            .take(100)
+            .map(i -> new ProducerRecord<>("order-events", i.toString(), "order-" + i))
+            .map(pr -> SenderRecord.create(pr, pr.key()));
+
+        KafkaSender.create(senderOptions)
+            .send(flux)
+            .doOnNext(record -> log.info("Correlation: {}", record.correlationMetadata()))
+            .subscribe();
+    }
+
+
+}
